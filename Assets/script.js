5add50e78cbd16dd4e5b493534c13916eba8d4bc
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-09-15 18:29:43 -0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,25 @@
-var requestWeather = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=37bc77bf6931bb85bef928e619baf386"
+const APIKey = "37bc77bf6931bb85bef928e619baf386";
+var forecast = document.getElementById("forecast");
+
+
+
+
+
+
+
+function getWeather(){
+    var requestWeather = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=37bc77bf6931bb85bef928e619baf386";
+
+    fetch(requestWeather)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        forecast.textContent = data.data.fact;
+        return;
+    })
+    }
+
+
+
+    https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=37bc77bf6931bb85bef928e619baf386
